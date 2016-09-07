7bfd565e2ec80fc4c4bb694e676c8346ceb6c3aa
xdrop <pastosdev@gmail.com>
2016-09-07 05:06:24 +0300

    Add AES Encrypt/Decrypt

---
+++ src/main/java/com/xdrop/passlock/crypto/AESEncrypt.java
@@ -0,0 +1,56 @@
+package com.xdrop.passlock.crypto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidParameterSpecException;
+
+public class AESEncrypt {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AESEncrypt.class);
+
+    private static final String cipherType = "AES/CBC/PKCS5Padding";
+
+
+    /**
+     * Encrypts a given payload using the secret key provided.
+     *
+     * @param payload The byte array of data to be encrypted
+     * @param key The key with which the data should be encrypted
+     * @return The encrypted payload and the IV vector
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+    public AESOutput encrypt(byte[] payload, SecretKey key) throws BadPaddingException, IllegalBlockSizeException {
+
+        try {
+
+            Cipher cipher = Cipher.getInstance(cipherType);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            AlgorithmParameters params = cipher.getParameters();
+            byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+
+            byte[] output = cipher.doFinal(payload);
+
+            return new AESOutput(output, iv);
+
+        } catch (NoSuchAlgorithmException |
+                NoSuchPaddingException |
+                InvalidKeyException |
+                InvalidParameterSpecException e) {
+
+            LOG.info("Failed to encrypt", e);
+            return null;
+
+        }
+
+    }
+
+
+}
---
+++ src/main/java/com/xdrop/passlock/crypto/AESOutput.java
@@ -0,0 +1,21 @@
+package com.xdrop.passlock.crypto;
+
+public class AESOutput {
+
+    private byte[] output;
+
+    private byte[] iv;
+
+    public AESOutput(byte[] output, byte[] iv) {
+        this.output = output;
+        this.iv = iv;
+    }
+
+    public byte[] getOutput() {
+        return output;
+    }
+
+    public byte[] getIv() {
+        return iv;
+    }
+}
