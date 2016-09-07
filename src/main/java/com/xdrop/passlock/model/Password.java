05f34efabb336c2bacf3a235fae0e14f4d4087e3
xdrop <pastosdev@gmail.com>
2016-09-07 04:44:09 +0300

    Password class switch to char[]

--- src/main/java/com/xdrop/passlock/model/Password.java
+++ src/main/java/com/xdrop/passlock/model/Password.java
@@ -4,11 +4,49 @@ import java.util.Date;
 
 public class Password {
 
-    private String password;
+    private char[] password;
     private Date date;
     private String description;
     private String ref;
     private String id;
 
+    public char[] getPassword() {
+        return password;
+    }
 
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
