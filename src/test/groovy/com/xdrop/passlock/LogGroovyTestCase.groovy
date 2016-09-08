0cb9ef914e76034e644740766d4ad86bd0f93342
xdrop <pastosdev@gmail.com>
2016-09-08 20:10:59 +0300

    Repair test build

--- src/test/groovy/com/xdrop/passlock/LogGroovyTestCase.groovy
+++ src/test/groovy/com/xdrop/passlock/LogGroovyTestCase.groovy
@@ -1,11 +1,12 @@
 package com.xdrop.passlock
 
 import org.apache.log4j.BasicConfigurator
+import org.junit.Ignore
 
+@Ignore
 class LogGroovyTestCase extends GroovyTestCase {
 
     static {
         BasicConfigurator.configure();
     }
-
 }
