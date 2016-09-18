21a36096f8a33b020d6ca3fae8a7b27cfa8f475f
xdrop <pastosdev@gmail.com>
2016-09-18 19:27:37 +0300

    Update install

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-ALIASNAME="plock"
-JARNAME="passlock.jar"
-BASEDIR=$(dirname "$0")
-JARPATH="$BASEDIR/$JARNAME"
-DST="/usr/bin/$ALIASNAME"
-echo "Symlinking $JARPATH to $DST"
-ln -s "$JARPATH" "$DST" 
+SOURCE=bin/plock
+TARGET="/usr/bin/plock"
+echo "Symlinking $SOURCE to $TARGET"
+ln -s "$SOURCE" $TARGET" 
