216a14ae38e70bffc3ca4086f88eb618a24cf547
xdrop <pastosdev@gmail.com>
2016-09-18 19:32:48 +0300

    Update packaging script

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ mv -f target/passlock-1.0-SNAPSHOT-jar-with-dependencies.jar bin/passlock.jar
 cd bin && cat javastub.sh passlock.jar > plock
 cd ..
 mkdir -p distribution/bin
-cp plock distribution/bin/plock
+cp bin/plock distribution/bin/plock
 mkdir -p distribution/config
 mv config/passlock.config distribution/config/passlock.config
 zip -r passlock.zip distribution/* 
